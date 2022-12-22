148ba19921dc3083fc35aab512bbc5383cff1eb0
AdasTamosiunas <121239917+AdasTamosiunas@users.noreply.github.com>
2022-12-22 20:12:40 +0200

    Add files via upload

---
+++ Application/Application/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using ApiClient;
+
+namespace ApiClientExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //  visu breeds listas
+            var breedsResponse = await ApiHelper.GetDogsAsync();
+            Console.WriteLine("Breeds:");
+            foreach (var breed in breedsResponse.Message) 
+            {
+                Console.WriteLine($"{breed.Key}"); 
+            }
+
+            // Subreed pasirinktam breed
+            Console.WriteLine("\nSubbreeds for poodle:");
+            var subBreedsResponse = await ApiHelper.GetSubBreedsAsync("poodle");
+            foreach (var subBreed in subBreedsResponse.Message)
+            {
+                Console.WriteLine(subBreed);
+            }
+
+            // Random nuotrauka pasirinktam breed
+            Console.WriteLine("\nRandom image of a poodle:");
+            var breedImageResponse = await ApiHelper.GetBreedImageAsync("poodle");
+            Console.WriteLine(breedImageResponse.Message);
+
+            // Random nuotrauka
+            Console.WriteLine("\nRandom image of any breed:");
+            var randomImageResponse = await ApiHelper.GetRandomImageAsync();
+            Console.WriteLine(randomImageResponse.Message);
+        }
+    }
+}
